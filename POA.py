4b64b2f43a7d40b10199f99d1e4b68bdc09c8807
shakedc1599 <47411973+shakedc1599@users.noreply.github.com>
2023-05-17 22:53:07 +0300

    Option 1 with comments

--- POA.py
+++ POA.py
@@ -1,28 +1,18 @@
 import sys
 from Cryptodome.Cipher import DES
-from Cryptodome.Util.Padding import pad, unpad
-
-
-def encrypt(padded_data, key, iv):
-    # Create a DES cipher object in CBC mode
-    cipher = DES.new(key, DES.MODE_CBC, iv)
-
-    # Encrypt the padded data
-    ciphertext = cipher.encrypt(padded_data)
-    return ciphertext
+from Cryptodome.Util.Padding import unpad
 
 
 def oracle(ciphertext, key, iv):
     try:
         cipher = DES.new(key, DES.MODE_CBC, iv)
         deciphered_text = cipher.decrypt(ciphertext)
-        unpadded_decrypted_text = unpad(deciphered_text, 8)
+        unpad(deciphered_text, 8)
         return True
     except ValueError:
         # print("Decryption error: Invalid padding!")
         return False
     except Exception as e:
-        print("Decryption error:", str(e))
         return False
 
 
@@ -30,88 +20,119 @@ def xor(a, b, c):
     return (a ^ b ^ c).to_bytes(1, 'big')
 
 
-def creat_next_xj(p_tag, c_i_minus_history, decipher_block_history, j):
+def creat_next_xj(p_tag, previous_ci, previous_block, j):
+    """
+    Creates the next Xj value.
+    """
+
     xj = []
     for x in range(j):
-        xj.append(xor(p_tag[0], c_i_minus_history[x], (decipher_block_history[x])[0]))
+        xj.append(xor(p_tag[0], previous_ci[x], (previous_block[x])[0]))
     return xj
 
 
 def reversed_list(old_list):
+    """
+    Reverses the given list.
+    """
     return old_list[::-1]
 
 
 def POA():
+
+    # Gets the variables from the user and converts them from Hex.
     ciphertext = bytes.fromhex(sys.argv[1])
     key = bytes.fromhex(sys.argv[2])
     iv = bytes.fromhex(sys.argv[3])
+
+    # Defines the block size.
     block_size = 8
 
-    # padded_data = pad(ciphertext, block_size)
-    # ciphertext = encrypt(padded_data, key, iv)
+    # Split the ciphertext into blocks.
     blocks = [ciphertext[x: x + block_size] for x in range(0, len(ciphertext), block_size)]
+
+    # Reverse the blocks.
     blocks = reversed_list(blocks)
-    plaintext_after_decrypt = []
+
+    # Create a list to store the decrypted blocks.
+    ciphertext_after_decryption = []
 
     for i in range(len(blocks)):
         second_block = blocks[i]
+
+        # If this is the last block, use the initialization vector as the first block.
         if i + 1 == len(blocks):
             first_block = iv
         else:
             first_block = blocks[i + 1]
 
-        xj_saved = []
-        ci_minus1_history = []
+        temp_xj = []
+
+        # Create a list to store the previous Ci values.
+        previous_ci__minus1 = []
         decipher_block = []
 
         for j in range(9):
+            # If this is the last iteration add the decipher block and break.
             if j == 8:
-                plaintext_after_decrypt.append(decipher_block)
+                ciphertext_after_decryption.append(decipher_block)
                 break
 
-            if not xj_saved:
+            # If there are no previous Xj values, create a list of all zeros.
+            if not temp_xj:
                 c = b'\x00' * (8 - j)
             else:
                 c = b'\x00' * (8 - j)
                 g = b''
 
-                for k in range(len(xj_saved)):
-                    g = g + xj_saved[k]
+                for k in range(len(temp_xj)):
+                    g = g + temp_xj[k]
                 c = c + g
 
+            # Create the p'.
             p_tag = (j + 1).to_bytes(1, 'big')
-            ci_minus1_current = first_block[7 - j]
+            current_ci_minus1 = first_block[7 - j]
 
+            # Iterate over all possible values for the current byte.
             for x in range(256):
                 cipher_modified = c + second_block
+
+                # Creat the new IV.
                 new_iv = cipher_modified[:8]
-                flag = oracle(cipher_modified[8:], key, new_iv)
-                if flag:
+
+                # If the decryption was successful.
+                if oracle(cipher_modified[8:], key, new_iv):
                     xj = c[7 - j]
                     p_tag2 = (j + 2).to_bytes(1, 'big')
-                    plaintext_last_byte = xor(p_tag[0], ci_minus1_current, xj)
+                    plaintext_last_byte = xor(p_tag[0], current_ci_minus1, xj)
                     decipher_block.append(plaintext_last_byte)
-                    ci_minus1_history.append(ci_minus1_current)
-                    xj_saved = creat_next_xj(p_tag2, ci_minus1_history, decipher_block, j + 1)
-                    xj_saved = reversed_list(xj_saved)
+                    previous_ci__minus1.append(current_ci_minus1)
+                    temp_xj = creat_next_xj(p_tag2, previous_ci__minus1, decipher_block, j + 1)
+                    temp_xj = reversed_list(temp_xj)
                     break
+
+                # Otherwise, set the current byte to the next possible value.
                 else:
                     c = bytearray(c)
                     c[7 - j] = x
                     c = bytes(c)
 
-    new_list = []
-    for blocks in plaintext_after_decrypt:
-        b = reversed_list(blocks)
-        b = b''.join(b)
-        new_list.append(b)
+    plaintext_blocks = []
 
-    new_list = reversed_list(new_list)
-    plaintext = b''.join(new_list)
+    # Go through each block and return the values in it to the original order and then combine them
+    for blocks in ciphertext_after_decryption:
+        new_block = reversed_list(blocks)
+        new_block = b''.join(new_block)
+        plaintext_blocks.append(new_block)
+
+    # Return the blocks to the correct order, unite them and remove the padding.
+    plaintext_blocks = reversed_list(plaintext_blocks)
+    plaintext = b''.join(plaintext_blocks)
     plaintext = unpad(plaintext, block_size)
-    plaintext = plaintext.decode()
 
-    print(f"Your word was: {plaintext}")
+    # Decode and print the plaintext.
+    plaintext = plaintext.decode()
+    print(plaintext)
 
 
 POA()
